f1516c96af62983f541e12454ca4ae210bc3c37e
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-10-15 16:28:23 +0800

    Update main.c

--- te/main.c
+++ te/main.c
@@ -1,15 +1,42 @@
-//
-//  main.c
-//  te
-//
-//  Created by s20181102929 on 2019/10/15.
-//  Copyright © 2019 s20181102929. All rights reserved.
-//
-
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#include <stdlib.h>
+#define ERROR 0
+struct BinTree
+{
+    int data;
+    struct BinTree * Left;
+    struct BinTree * Right;
+};
+void create(struct BinTree * *BT)
+{
+    int n;
+    scanf("%d",&n);
+    if(n!=ERROR)
+    {
+        (*BT)=(struct BinTree *)malloc(sizeof(struct BinTree));
+        (*BT)->data=n;
+        create(&((*BT)->Left));
+        create(&((*BT)->Right));
+    }
+}
+void print(struct BinTree * BT,int h)
+{
+    if(BT)
+    {
+        print(BT->Right,h+1);
+        for(int i=0;i<h;i++)
+        {
+            printf("  ");
+        }
+        printf("%d\n",BT->data);
+        print(BT->Left,h+1);
+    }
+}
+int main()
+{
+    struct BinTree b,*BT;
+    BT=&b;
+    create(&BT);
+    print(BT,1);
     return 0;
 }
